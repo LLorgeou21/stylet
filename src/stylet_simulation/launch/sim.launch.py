a8467f655e2ce3158e71704ba332ff127e2b7afe
Lilian Lorgeou <lilian.lorgeou@gmail.com>
2026-07-02 15:39:03 +0200

    feat: complete Gazebo scene with robot, needle and target

---
+++ src/stylet_simulation/launch/sim.launch.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import os
+import xacro
+from launch import LaunchDescription
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    # --- Chemins ---
+    world_path = os.path.join(
+        get_package_share_directory("stylet_simulation"), "worlds", "stylet.world"
+    )
+    xacro_file = os.path.join(
+        get_package_share_directory("stylet_description"), "urdf", "robot.urdf.xacro"
+    )
+    robot_description = xacro.process_file(xacro_file).toxml()
+
+    # --- Bloc 1 : où Gazebo doit chercher model://target ---
+    set_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=os.path.join(get_package_share_directory("stylet_simulation"), "models")
+        + ":"
+        + os.path.join(get_package_share_directory("stylet_simulation"), "worlds")
+        + ":"
+        + os.environ.get("GZ_SIM_RESOURCE_PATH", ""),
+    )
+
+    # --- Bloc 2 : démarrer Gazebo avec notre monde ---
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py"
+            )
+        ),
+        launch_arguments={"gz_args": world_path}.items(),
+    )
+
+    # --- Bloc 3 : publier robot_description (comme en 1.3/1.4) ---
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}],
+    )
+
+    # --- Bloc 4 : faire apparaître le robot DANS Gazebo ---
+    spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=["-topic", "/robot_description", "-name", "ur5e"],
+    )
+
+    # --- Assemblage final : TOUT doit être dans cette liste ---
+    return LaunchDescription(
+        [
+            set_resource_path,
+            gz_sim,
+            robot_state_publisher,
+            spawn_robot,
+        ]
+    )
